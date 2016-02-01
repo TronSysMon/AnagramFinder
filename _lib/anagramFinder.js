0ab60ab5101d540914071baa4abd83d5d908f831
TronSysMon <TronSysMon@gmail.com>
2016-01-31 23:53:51 -0500

    ==READY FOR RELEASE== Finished coding and testing script, all requirements met, successfully tested.

--- _lib/anagramFinder.js
+++ _lib/anagramFinder.js
@@ -1,7 +1,101 @@
+/* Parker Baine | 29 Jan 2016 */
+
+/*================================================================================*/
+
+function func_AF_ParseInputString(str_psParsePhrase)
+{
+	var returnVal = "";
+	
+	if(str_psParsePhrase)
+	{
+		//Cast arguments as explicit string values
+		var str_tempParsePhrase = String(str_psParsePhrase);
+		
+		//Get rid of all other whitespace
+		str_tempParsePhrase = str_tempParsePhrase.replace(/\s+/g, "");
+		
+		//Trim whitespace
+		str_tempParsePhrase = str_tempParsePhrase.trim();
+		
+		returnVal = str_tempParsePhrase;
+		
+	}	
+	
+	return returnVal;
+}
+
+/*================================================================================*/
 
 function areAnagrams(s1, s2)
 {
-	
-	return false;
-	
+	//Check for NULL
+	if((s1)&&(s2))
+	{
+		
+		//Parse and scrub input
+		var str_tempS1 = func_AF_ParseInputString(s1);
+		var str_tempS2 = func_AF_ParseInputString(s2);
+		
+		//Check the length of both strings
+		var i_s1Len = str_tempS1.length;
+		var i_s2Len = str_tempS2.length;
+				
+		//Make sure both strings are same len, else break
+		if(i_s1Len == i_s2Len)
+		{	
+		
+			//Convert input strings to arrays of chars
+			var arr_s1Chars = str_tempS1.split('');
+			var arr_s2Chars = str_tempS2.split('');
+			
+			//Set up comparison lengths
+			var i_inputLen = ((i_s1Len + i_s2Len) / 2);
+			var i_matchCount = 0;
+		
+			//Nested loop through both arrays
+			for (i_s1Count = 0; i_s1Count < arr_s1Chars.length; i_s1Count++) 
+			{ 
+				for (i_s2Count = 0; i_s2Count < arr_s2Chars.length; i_s2Count++)
+				{
+					if(arr_s1Chars[i_s1Count] ==  arr_s2Chars[i_s2Count])
+					{
+						//If a match is found
+						//1) increment the match count
+						i_matchCount++;
+						
+						//2) set the current array element equal to blank
+						arr_s2Chars[i_s2Count] = "";
+						
+						//3) break out of the inner loop
+						break;
+					}
+				}
+			}
+			
+			//Compare match count to input length(s)
+			if(i_matchCount == i_inputLen)
+			{
+				//true is both integers are equal
+				return true;
+			}
+			else
+			{
+				//false is integers are not equal
+				return false;
+			}
+			
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;		
+	}
+
 }
+
+/*================================================================================*/
+
--- index.script.js
+++ index.script.js
@@ -1,10 +1,91 @@
-$(document).ready(function(){
+/* Parker Baine | 29 Jan 2016 */
 
-   // Local Scripts
-   
-   if(!areAnagrams("Yes","No"))
+/*================================================================================*/
+
+//Document onload test for AnagramFinder
+$(document).ready(function()
+{
+   //Test AnagramFinder, throw error if fails unit test
+    if(!areAnagrams("abcdABCD1234!@#$","!@#$1234ABCDabcd"))
    {
-	   //alert("Hello World!");
+	   alert("*AnagramFinder.js error*");
    }
 
 });
+
+/*================================================================================*/
+
+//Parse and scrub string output values.
+//Copy of func_AF_ParseInputString() from anagramFinder.js
+//which is treated as if it were a private function.
+function ParseInputString(str_psParsePhrase)
+{
+	var returnVal = "";
+	
+	if(str_psParsePhrase)
+	{
+		//Cast arguments as explicit string values
+		var str_tempParsePhrase = String(str_psParsePhrase);
+		
+		//Get rid of all other whitespace
+		str_tempParsePhrase = str_tempParsePhrase.replace(/\s+/g, "");
+		
+		//Trim whitespace
+		str_tempParsePhrase = str_tempParsePhrase.trim();
+		
+		returnVal = str_tempParsePhrase;
+		
+	}	
+	
+	return returnVal;
+}
+
+/*================================================================================*/
+
+//Compare button to run the AnagramFinder and update the output area
+$("#AF_button_main_container_compare").click(function() 
+{
+	
+	//Get the input values from the text boxes
+	var s1 = $("#AF_textbox_main_container_s1").val();
+	var s2 = $("#AF_textbox_main_container_s2").val();
+	var results = "";
+	
+	//Run the AnagramFinder and test the results
+	if(areAnagrams(s1,s2))
+	{
+	   results = "<p class='AF_p_main_container_right_results_match'><strong>==Match==</strong></p>";
+	}
+	else
+	{
+	   results = "<p class='AF_p_main_container_right_results_nomatch'><strong>==No Match==</strong></p>";
+	}
+	
+	//Update the HTML output area with results
+	$(".AF_div_main_container_right_results").html
+	(
+		results + 
+		"<p class='AF_p_main_container_right_results_text'>First Phrase:</p>" +
+		"<p class='AF_p_main_container_right_results_output'>" + 
+		ParseInputString(s1) +
+		"</p>" + 
+		"<p class='AF_p_main_container_right_results_text'>Second Phrase:</p>" +
+		"<p class='AF_p_main_container_right_results_output'>" + 
+		ParseInputString(s2) +
+		"</p>" + 
+		""
+	);
+});
+
+/*================================================================================*/
+
+//Reset button to reset input and output
+$("#AF_button_main_container_reset").click(function() 
+{	
+	$(".AF_div_main_container_right_results").html("<p>...</p>");
+	$("#AF_textbox_main_container_s1").val("");
+	$("#AF_textbox_main_container_s2").val("");
+});
+
+/*================================================================================*/
+
